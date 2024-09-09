9a3fd801bd13a87a0fc8757f3397efa05f71ee92
EsraKaban <esraandio@gmail.com>
2024-09-09 11:12:47 +0700

    Meshtriple

---
+++ Meshtriple.m
@@ -0,0 +1,6 @@
+t = 0:pi/20:4*pi;
+[X,Y,Z] = cylinder(2*sin(t));
+subplot(2,2,1); mesh(X); title('X');
+subplot(2,2,2); mesh(Y); title('Y');
+subplot(2,2,3); mesh(Z); title('Z');
+subplot(2,2,4); mesh(X,Y,Z); title('X,Y,Z');
